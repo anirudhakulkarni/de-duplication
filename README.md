73f4eff8e0d1d4f4536092e9f1fa73c7587d485e
Anirudha Kulkarni <kulkarnianirudha8@gmail.com>
2022-10-30 22:24:52 +0400

    robust to directories

---
+++ README.md
@@ -0,0 +1,8 @@
+# Deduplication API
+
+## Introduction
+Deduplcation saves the storage
+
+http://seoonlinesource.blogspot.com/2012/01/how-dropbox-sacrifices-user-privacy-for.html
+
+https://www.cs.bu.edu/~goldbe/teaching/HW55812/sanaz.pdf
--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,51 @@ use std::hash::{Hash, Hasher};
 use std::io::{Read, Write};
 use std::path::Path;
 use std::fs;
+use sha2::{Sha256, Digest};
 // define constant
-const CHUNK_SIZE: usize = 1024 * 1024;
+const CHUNK_SIZE: usize = 1024 *1024;
+
+// constants: database path, state path, map1 path, map2 path
+const DATABASE_PATH: &str = "~/database/";
+// state path is database path + state
+const STATE_PATH: &str = "~/database/state";
+// map1 path is database path + map1
+const MAP1_PATH: &str = "~/database/map1";
+// map2 path is database path + map2
+const MAP2_PATH: &str = "~/database/map2";
+
 
 //get hash of an vector (input type is Vec<u8>)
-fn get_hash(input: &Vec<u8>) -> u64 {
+fn get_hash(input: &Vec<u8>) -> u64{
+    // let mut hasher = Sha256::new();
     let mut hasher = DefaultHasher::new();
+    // hasher.update(input);
+    // hasher.finalize() 
     input.hash(&mut hasher);
     let hash = hasher.finish();
     hash
 }
+// reads the state of last chunk index and returns
 fn get_last_chunk_index() -> u64 {
     // if state.txt does not exist create it
-    let path = Path::new("state.txt");
+    let path = Path::new(STATE_PATH);
     if !path.exists() {
-        let mut file = File::create("state.txt").unwrap();
+        let mut file = File::create(STATE_PATH).unwrap();
         file.write_all(b"0").unwrap();
         return 0;
     }
     // read state.txt
-    let mut file = File::open("state.txt").unwrap();
+    let mut file = File::open(STATE_PATH).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     let last_chunk_index = contents.parse::<u64>().unwrap();
     last_chunk_index + 1
-    // File::open("state.txt").unwrap().read_to_string(&mut String::new()).unwrap()
+    // File::open(STATE_PATH).unwrap().read_to_string(&mut String::new()).unwrap()
 }
 
 
+
+// Check if the map exists and if it does load it
 fn checkifexist(file_path: &str)
 // return hashmap of string to vector of strings
 -> HashMap<String, Vec<String>> {
@@ -59,42 +76,51 @@ fn checkifexist(file_path: &str)
         map.insert(key.to_string(), values);
 
     }
-
     map
 }
 
+
+// setups the directories if they do not exist
 fn setup(){
     // if state.txt does not exist create it and set it to 0
-    let path = Path::new("state.txt");
+    let path = Path::new(DATABASE_PATH);
     if !path.exists() {
-        let mut file = File::create("state.txt").unwrap();
+        std::fs::create_dir(path).unwrap();
+    }
+
+    let path = Path::new(STATE_PATH);
+    if !path.exists() {
+        let mut file = File::create(STATE_PATH).unwrap();
         file.write_all(b"0").unwrap();
     }
     // if map1.txt does not exist create it
-    let path = Path::new("map1.txt");
+    let path = Path::new(MAP1_PATH);
     if !path.exists() {
         let mut file = File::create(path).unwrap();
     }
     // if map2.txt does not exist create it
-    let path = Path::new("map2.txt");
+    let path = Path::new(MAP2_PATH);
     if !path.exists() {
         let mut file = File::create(path).unwrap();
     }
     // if chunks directory does not exist create it
-    let path = Path::new("chunks");
+    let path = Path::new(DATABASE_PATH).join("chunks");
     if !path.exists() {
         std::fs::create_dir(path).unwrap();
     }
     
 }
+
+// reset button. Deletes database 
 fn clear(){
-    fs::remove_file("map1.txt");
-    fs::remove_file("map2.txt");
-    fs::remove_file("state.txt");
-    fs::remove_dir_all("chunks");
-    
+    fs::remove_file(MAP1_PATH);
+    fs::remove_file(MAP2_PATH);
+    fs::remove_file(STATE_PATH);
+    // fs::remove_dir_all(DATABASE_PATH+"/chunks");
+    fs::remove_dir(DATABASE_PATH);
 }
 
+// given a byte file get the chunks and hashes
 fn get_hashes
 // return vector of tupe {chunk, hash} from vector of bytes
 (input: &Vec<u8>) -> 
@@ -115,6 +141,8 @@ Vec<(Vec<u8>, u64)> {
     
     hashes
 }
+
+// saves the entire file to the database
 pub fn save_file(path: &str) {
     setup();
     // let mut file = File::open(path).unwrap();
@@ -126,8 +154,8 @@ pub fn save_file(path: &str) {
     // get hashes of file
     let hashes = get_hashes(&buffer);
 
-    let mut hashtochunk = checkifexist("map1.txt");
-    let mut filetohashes = checkifexist("map2.txt");
+    let mut hashtochunk = checkifexist(MAP1_PATH);
+    let mut filetohashes = checkifexist(MAP2_PATH);
 
     // append hashes to csv file with as filename, hasharray
     // if file exists append to it
@@ -136,7 +164,7 @@ pub fn save_file(path: &str) {
         .write(true)
         .append(true)
         .create(true)
-        .open("map2.txt")
+        .open(MAP2_PATH)
         .unwrap();
     file.write_all(path.as_bytes()).unwrap();
     file.write_all(b",").unwrap();
@@ -159,7 +187,7 @@ pub fn save_file(path: &str) {
 
     for chunk_hash in &hashes{
         if !hashtochunk.contains_key(&chunk_hash.1.to_string()) {
-            let mut file = File::create(format!("chunks/{}.chunk", i)).unwrap();
+            let mut file = File::create(format!("{}chunks/{}.chunk",DATABASE_PATH, i)).unwrap();
             file.write_all(&chunk_hash.0).unwrap();
             hashtochunk.insert(chunk_hash.1.to_string(), vec![format!("{}.chunk", i)]);
             // write to map1.txt
@@ -167,7 +195,7 @@ pub fn save_file(path: &str) {
                 .write(true)
                 .append(true)
                 .create(true)
-                .open("map1.txt")
+                .open(MAP1_PATH)
                 .unwrap();
             file.write_all(chunk_hash.1.to_string().as_bytes()).unwrap();
             file.write_all(b",").unwrap();
@@ -178,14 +206,14 @@ pub fn save_file(path: &str) {
     }
 
     // save last chunk index
-    let mut file = File::create("state.txt").unwrap();
+    let mut file = File::create(STATE_PATH).unwrap();
     file.write_all(i.to_string().as_bytes()).unwrap();
 
 }
 
 pub fn load_file(path: &Path) -> std::io::Result<Vec<u8>> {
-    let mut hashtochunk= checkifexist("map1.txt");
-    let mut filetohashes = checkifexist("map2.txt");
+    let mut hashtochunk= checkifexist(MAP1_PATH);
+    let mut filetohashes = checkifexist(MAP2_PATH);
     // println!("{:?}", hashtochunk);
     // println!("{:?}", filetohashes);
     
@@ -197,7 +225,7 @@ pub fn load_file(path: &Path) -> std::io::Result<Vec<u8>> {
         // println!("{:?}", hash);      
         // println!("{:?}",hashtochunk.get(hash).unwrap()[0]);
 
-        let mut file = File::open(format!("chunks/{}", hashtochunk.get(hash).unwrap()[0] )).unwrap();
+        let mut file = File::open(format!("{}chunks/{}",DATABASE_PATH, hashtochunk.get(hash).unwrap()[0] )).unwrap();
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer).unwrap();
         chunks.push(buffer);
